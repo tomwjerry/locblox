0208da48c8b0d629e65ed70a4c7a63f8a42bc9c6
Tom Elssjö <tom@jatech.se>
2021-01-19 20:02:56 +0100

    Editable blocks

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <template>
+    <text-block v-for="(content, index) in contentList"
+        :key="index"
+        :edit="edit"
+        :value="content"
+    ></text-block>
 
+    <button @click="edit = !edit" type="button">Edit</button>
+    <button @click="addElement()" type="button">Add</button>
 </template>
 
 <script>
-    
+    import TextBlock from './components/TextBlock.vue';
+
+    export default {
+        data() {
+            return {
+                contentList: ['&nbsp;'],
+                edit: false
+            };
+        },
+        components: {
+            'text-block': TextBlock
+        },
+        methods: {
+            addElement() {
+                this.contentList.push('&nbsp;');
+            }
+        }
+    };
 </script>
---
+++ src/components/TextBlock.vue
@@ -0,0 +1,12 @@
+<template>
+    <div :contenteditable="edit" v-html="value"></div>
+</template>
+
+<script>
+export default {
+    props: {
+        edit: Boolean,
+        value: String
+    }
+};
+</script>
